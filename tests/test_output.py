a7dc64bf9011d42d04654f00e264c5838943dc2d
justin66744 <justintt66744@gmail.com>
2025-04-23 16:04:07 -0700

    -Began unit testing -Tested for a valid file with valid text

---
+++ tests/test_output.py
@@ -0,0 +1,8 @@
+import unittest
+from output import Simulation
+import io
+import sys
+import tempfile
+
+class TestOutput(unittest.TestCase):
+    pass
--- tests/test_project1.py
+++ tests/test_project1.py
@@ -1,6 +1,40 @@
+import os
 import unittest
+from project1 import file_read
+import pathlib
+import tempfile
 
 class TestProj1(unittest.TestCase):
-    pass
+    def create_temp_file(self, text):
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            temp_file.write(text)
+            return temp_file.name
 
+    def test_file_with_valid_file(self):
+        text = """LENGTH 1500
+                  DEVICE 1
+                  DEVICE 2
+                  PROPAGATE 1 2 150
+                  PROPAGATE 2 1 200 
+                  ALERT 1 test 200
+                  CANCEL 1 test 450
+                  """
+        temp_path = self.create_temp_file(text)
+        try:
+            path = pathlib.Path(temp_path)
+            length, devices, propagates, alerts, cancellations = file_read(path)
+            
+            self.assertEqual(length, 1500)
+            self.assertEqual(devices, [1, 2])
+            self.assertEqual(propagates, {'1': ('2', '150'), '2': ('1', '200')})
+            self.assertEqual(alerts, {'1': ('test', '200')})
+            self.assertEqual(cancellations, {'1': ('test', '450')})
+        finally:
+            if os.path.exists(temp_path):
+                os.unlink(temp_path)
 
+    def test_file_with_invalid_file(self):
+        text = """LENGTH one hundred
+                  DEVICE one
+                  DEVICE two
+                  """

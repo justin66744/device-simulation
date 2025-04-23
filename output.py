3020df9a3548daa95e1056f8b5d0404c4686bd53
justin66744 <justintt66744@gmail.com>
2025-04-23 15:03:59 -0700

    -Implementation passes both sample_input and the sanity checker -Working on unit tests

--- output.py
+++ output.py
@@ -17,16 +17,20 @@ class Simulation:
             self._cancel_times.append(cancellation[1])
         self._cancel_times.sort()
 
+        self._cancelled_alerts = []
+
     def prop_alert(self):
+        msgs = []
         curr_alert = []
         curr_props = []
         for dev_id, (desc, time) in self._alerts.items():
-            curr_alert.append((dev_id, desc))
+            curr_alert.append((dev_id, (desc, time)))
 
         for dev_id, (propped_to, delay) in self._propagates.items():
             curr_props.append((dev_id, (propped_to, delay)))
 
-        for dev_id, desc in curr_alert:
+        for dev_id, (desc, time) in curr_alert:
+            self._time = int(time)
             if dev_id in self._propagates.keys():
                 delay = self._propagates[dev_id][1]
                 new_time = self._time
@@ -35,33 +39,41 @@ class Simulation:
                     for prop in curr_props:
                         new_time += int(prop[1][1])
                         if new_time < self._length:
-                            print(f"@{self._time}: #{prop[0]} SENT ALERT TO #{prop[1][0]}: {desc}")
-                            print(f"@{new_time}: #{prop[1][0]} RECEIVED ALERT FROM #{prop[0]}: {desc}")
+                            msgs.append(f"@{self._time}: #{prop[0]} SENT ALERT TO #{prop[1][0]}: {desc}")
+                            msgs.append(f"@{new_time}: #{prop[1][0]} RECEIVED ALERT FROM #{prop[0]}: {desc}")
                         self._time = new_time
+        return msgs
 
 
 
     def prop_cancel(self):
+        msgs = []
         curr_cancel = []
         curr_props = []
         for dev_id, (desc, time) in self._cancellations.items():
-            curr_cancel.append((dev_id, desc))
+            curr_cancel.append((dev_id, (desc, time)))
 
         for dev_id, (propped_to, delay) in self._propagates.items():
             curr_props.append((dev_id, (propped_to, delay)))
 
-        for dev_id, desc in curr_cancel:
+        for dev_id, (desc, time) in curr_cancel:
+            self._time = int(time)
             if dev_id in self._propagates.keys():
                 delay = self._propagates[dev_id][1]
                 new_time = self._time
 
                 while new_time < self._length:
+                    if len(curr_props) == len(self._cancelled_alerts):
+                        break
                     for prop in curr_props:
                         new_time += int(prop[1][1])
                         if new_time < self._length:
-                            print(f"@{self._time}: #{prop[0]} SENT CANCELLATION TO #{prop[1][0]}: {desc}")
-                            print(f"@{new_time}: #{prop[1][0]} RECEIVED CANCELLATION FROM #{prop[0]}: {desc}")
+                            msgs.append(f"@{self._time}: #{prop[0]} SENT CANCELLATION TO #{prop[1][0]}: {desc}")
+                            msgs.append(f"@{new_time}: #{prop[1][0]} RECEIVED CANCELLATION FROM #{prop[0]}: {desc}")
                         self._time = new_time
+                        self._cancelled_alerts.append(prop)
+        return msgs
+
 
 
 
@@ -76,8 +88,59 @@ class Simulation:
             if len(self._alert_times) == 0 and len(self._cancel_times) == 0:
                 break
 
+            raw_msgs = []
 
-            self.prop_alert()
-            self.prop_cancel()
+            cancelled_msgs = self.prop_cancel()
+            raw_msgs.extend(cancelled_msgs)
+            self._time = 0
+            alert_msgs = self.prop_alert()
+            raw_msgs.extend(alert_msgs)
+
+            def timestamp(message):
+                return int(message.split('@')[1].split(':')[0])
+
+            raw_msgs.sort(key=timestamp)
+
+            final_msgs = []
+            sent_cancellations = {}
+            received_cancellations = {}
+
+            for msg in raw_msgs:
+                if 'CANCELLATION' in msg:
+                    pieces = msg.split()
+                    time = timestamp(msg)
+                    if 'SENT' in msg:
+                        from_dev = pieces[1][1:]
+                        to_dev = pieces[5][1:]
+                        sent_cancellations[(from_dev, to_dev)] = time
+                    elif 'RECEIVED' in msg:
+                        to_dev = pieces[1][1:]
+                        from_dev = pieces[5][1:]
+                        received_cancellations[(to_dev, from_dev)] = time
+
+            for msg in raw_msgs:
+                if 'ALERT' in msg:
+                    pieces = msg.split()
+                    time = timestamp(msg)
+
+                    if 'SENT' in msg:
+                        from_dev = pieces[1][1:]
+                        to_dev = pieces[5][1:]
+                        if (from_dev, to_dev) in sent_cancellations:
+                            if time >= sent_cancellations[(from_dev, to_dev)]:
+                                continue
+                    elif 'RECEIVED' in msg:
+                        to_dev = pieces[1][1:]
+                        from_dev = pieces[5][1:]
+                        if (to_dev, from_dev) in received_cancellations:
+                            if time >= received_cancellations[(to_dev, from_dev)]:
+                                continue
+
+                final_msgs.append(msg)
+
+            final_msgs.sort(key = timestamp)
+
+            for msg in final_msgs:
+                print(msg)
 
         print(f"@{self._length}: END")

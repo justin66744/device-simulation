c05589bbf653284a81e20c44ac3d69ef59e7fed3
justin66744 <justintt66744@gmail.com>
2025-04-25 15:09:40 -0700

    -Fixed alert & cancel at the same time test

--- tests/test_output.py
+++ tests/test_output.py
@@ -95,23 +95,23 @@ class TestOutput(unittest.TestCase):
         self.assertIn('@200: #2 SENT CANCELLATION TO #3: alert2', output)
         self.assertIn('@250: #3 RECEIVED CANCELLATION FROM #2: alert2', output)
 
-    # def test_alert_and_cancel_at_same_time(self):
-    #     devices = [1, 2]
-    #     propagates = {'1': ('2', '50')}
-    #     alerts = {'1': ('alert', '100')}
-    #     cancellations = {'1': ('alert', '100')}
-    #
-    #     test = Simulation(300, devices, propagates, alerts, cancellations)
-    #
-    #     with io.StringIO() as vals, contextlib.redirect_stdout(vals):
-    #         test.run()
-    #         output = vals.getvalue()
-    #
-    #     self.assertIn('@100: #1 SENT ALERT TO #2: alert', output)
-    #     self.assertIn('@100: #1 SENT CANCELLATION TO #2: alert', output)
-    #     self.assertIn('@150: #2 RECEIVED ALERT FROM #1: alert', output)
-    #     self.assertIn('@150: #2 RECEIVED CANCELLATION FROM #1: alert', output)
-    #     self.assertIn('@300: END', output)
+    def test_alert_and_cancel_at_same_time(self):
+        devices = [1, 2]
+        propagates = {'1': ('2', '50'), '2': ('1', '50')}
+        alerts = {'1': ('alert', '100')}
+        cancellations = {'1': ('alert', '100')}
+
+        test = Simulation(300, devices, propagates, alerts, cancellations)
+
+        with io.StringIO() as vals, contextlib.redirect_stdout(vals):
+            test.run()
+            output = vals.getvalue()
+
+        self.assertIn('@100: #1 SENT ALERT TO #2: alert', output)
+        self.assertIn('@100: #1 SENT CANCELLATION TO #2: alert', output)
+        self.assertIn('@150: #2 RECEIVED ALERT FROM #1: alert', output)
+        self.assertIn('@150: #2 RECEIVED CANCELLATION FROM #1: alert', output)
+        self.assertIn('@300: END', output)
 
 
 
